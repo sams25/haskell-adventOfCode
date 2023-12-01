8a864d28bd62bc41fe304f224e5c6b57961466a1
Anindya Sharma <anindya.sharma1@gmail.com>
2023-12-01 16:02:14 +0000

    cleaned code

--- lib/Day1.hs
+++ lib/Day1.hs
@@ -21,24 +21,22 @@ solution1 firstDigit lastDigit input =
           b = lastDigit xs
       in 10 * a + b
 
-solution1a = solution1 firstDigitVanilla (\xs -> firstDigitVanilla (reverse xs))
-solution1b = solution1 (firstDigitWithWords id) (\xs -> firstDigitWithWords reverse (reverse xs))
+solution1a = solution1 firstDigitVanilla (firstDigitVanilla . reverse)
+solution1b = solution1 (firstDigitWithWords id) (firstDigitWithWords reverse . reverse)
 
 firstDigitVanilla :: String -> Int
 firstDigitVanilla (x:xs) = if isDigit x then digitToInt x else firstDigitVanilla xs
 
 firstDigitWithWords :: (String -> String) -> String ->  Int
-firstDigitWithWords preOperation all@(x:xs) = 
-  if isDigit x 
-  then digitToInt x 
-  else if (preOperation "one"  ) `isPrefixOf` all then 1
-  else if (preOperation "two"  ) `isPrefixOf` all then 2
-  else if (preOperation "three") `isPrefixOf` all then 3
-  else if (preOperation "four" ) `isPrefixOf` all then 4
-  else if (preOperation "five" ) `isPrefixOf` all then 5
-  else if (preOperation "six"  ) `isPrefixOf` all then 6
-  else if (preOperation "seven") `isPrefixOf` all then 7
-  else if (preOperation "eight") `isPrefixOf` all then 8
-  else if (preOperation "nine" ) `isPrefixOf` all then 9
-  else firstDigitWithWords preOperation xs
-
+firstDigitWithWords preOperation all@(x:xs)
+  | isDigit x = digitToInt x
+  | preOperation "one"   `isPrefixOf` all = 1
+  | preOperation "two"   `isPrefixOf` all = 2
+  | preOperation "three" `isPrefixOf` all = 3
+  | preOperation "four"  `isPrefixOf` all = 4
+  | preOperation "five"  `isPrefixOf` all = 5
+  | preOperation "six"   `isPrefixOf` all = 6
+  | preOperation "seven" `isPrefixOf` all = 7
+  | preOperation "eight" `isPrefixOf` all = 8
+  | preOperation "nine"  `isPrefixOf` all = 9
+  | otherwise = firstDigitWithWords preOperation xs
