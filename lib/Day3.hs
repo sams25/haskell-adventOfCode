38fd1d8be8debbab77d0f12eb33ec991cfe30d95
Anindya Sharma <anindya.sharma1@gmail.com>
2023-12-03 21:27:54 +0000

    made day 3 more efficient

--- lib/Day3.hs
+++ lib/Day3.hs
@@ -5,27 +5,37 @@ module Day3
   )
   where
 
-import Math.Geometry.GridInternal
-import Math.Geometry.Grid.Octagonal
 import Data.Char (isDigit, digitToInt)
 import Data.List (elemIndex)
 import Data.Maybe (fromJust)
 
-solution getAnswer input =
-  let
-    numRows = 1 + count '\n' input
-    numCols = fromJust (elemIndex '\n' input)
-    grid = rectOctGrid numRows numCols
-  in
-    getAnswer grid (parseInput input)
+solution getAnswer input = show (sum (getAnswer (parseInput input)))
 
-solutionA = solution getValidParts
+solutionA = solution getValidPartNums
 solutionB = solution getGearRatios
 
--- Parsing the grid
-type Part = (Int, [Index RectOctGrid])
-type Symbol = (Char, Index RectOctGrid)
+-- X Y co-ordinates
+type Position = (Int, Int)
+-- <beginning position> <ending position>
+type Rect = (Position, Position)
+-- <number> <rectangle spanned by the digits in the number>
+type Part = (Int, Rect)
+-- <character> <position of character>
+type Symbol = (Char, Position)
 
+-- Is a position within a rectangle?
+inRect :: Rect -> Position -> Bool
+inRect ((begX, begY), (endX, endY)) (x, y) = begX <= x && x <= endX && begY <= y && y <= endY
+
+-- Is a position around a rectangle?
+aroundRect :: Rect -> Position -> Bool
+aroundRect rect = inRect (getSurroundingRect rect)
+
+-- Gets the start and end of a rectangle of distance 1 around a given rectangle
+getSurroundingRect :: Rect -> Rect
+getSurroundingRect ((begX, begY), (endX, endY)) = ((begX - 1, begY - 1), (endX + 1, endY + 1))
+
+-- Parsing the grid
 parseInput :: String -> ([Part], [Symbol])
 parseInput xs = parseInputAcc 1 1 xs [] [] Nothing
   where
@@ -33,44 +43,39 @@ parseInput xs = parseInputAcc 1 1 xs [] [] Nothing
     parseInputAcc _ _ [] parts syms Nothing = (parts, syms)
     parseInputAcc _ _ [] parts syms (Just part) = (part : parts, syms)
     -- Within a part number
-    parseInputAcc r c  (x : xs) parts syms (Just part@(num, indices))
-      | isDigit x = parseInputAcc r (c + 1) xs parts syms (Just (num * 10 + digitToInt x, (r, c) : indices))
+    parseInputAcc r c  (x : xs) parts syms (Just part@(num, (beg, end)))
+      | isDigit x = parseInputAcc r (c + 1) xs parts syms (Just (num * 10 + digitToInt x, (beg, (r, c))))
       | x == '.'  = parseInputAcc r (c + 1) xs (part : parts) syms Nothing
       | x == '\n' = parseInputAcc (r + 1) 1 xs (part : parts) syms Nothing
       | otherwise = parseInputAcc r (c + 1) xs (part : parts) ((x, (r, c)) : syms) Nothing
     -- Outside a part number
     parseInputAcc r c (x : xs) parts syms Nothing
-      | isDigit x = parseInputAcc r (c + 1) xs parts syms (Just (digitToInt x, [(r, c)]))
+      | isDigit x = parseInputAcc r (c + 1) xs parts syms (Just (digitToInt x, ((r, c), (r, c))))
       | x == '.'  = parseInputAcc r (c+1) xs parts syms Nothing
       | x == '\n' = parseInputAcc (r + 1) 1 xs parts syms Nothing
       | otherwise = parseInputAcc r (c + 1) xs parts ((x, (r, c)) : syms) Nothing
 
--- Utility functions
-count :: Eq a => a -> [a] -> Int
-count thing _ = foldl (\acc elem -> if elem == thing then acc + 1 else acc) 0 _
-
-anyWithin :: Eq a => [a] -> [a] -> Bool
-anyWithin [] _ = False
-anyWithin (x:xs) check = x `elem` check || anyWithin xs check
-
 -- Problem logic
-getValidParts :: RectOctGrid -> ([Part], [Symbol]) -> String
-getValidParts grid (parts, syms) =
+getValidPartNums :: ([Part], [Symbol]) -> [Int]
+getValidPartNums (parts, syms) =
   let
     symbolLocs = map snd syms
-    validParts = [ fst p | p <- parts, anyWithin (neighboursOfSet grid (snd p)) symbolLocs]
+    nearSymbol rect = any (aroundRect rect) symbolLocs
   in
-    show (sum validParts)
+    [p | (p, rect) <- parts, nearSymbol rect]
 
-getGearRatios :: RectOctGrid -> ([Part], [Symbol]) -> String
-getGearRatios grid (parts, syms) =
+getGearRatios :: ([Part], [Symbol]) -> [Int]
+getGearRatios (parts, syms) =
   let
     starLocs = [snd s | s <- syms, fst s == '*']
-    partNeighbours starLoc =
-      [ fst p | p <- parts, starLoc `elem` neighboursOfSet grid (snd p)]
-    potentialGearRatios =
-      [ partNeighbours (snd s) | s <- syms, fst s == '*' ]
-    gearRatios =
-      [ product ps | ps <- potentialGearRatios, length ps == 2 ]
+    -- Returns the true gear ratio for a valid location, and 0 otherwise
+    gearRatio :: Position -> Int
+    gearRatio loc =
+      let
+        partNumsNearby loc (p, rect) acc@(num, ps) = if aroundRect rect loc then (num + 1, p : ps) else acc
+        (numParts, partNums) = foldr (partNumsNearby loc) (0, []) parts
+        secondOfList (x:y:xs) = y
+      in
+        if numParts == 2 then head partNums * secondOfList partNums else 0
   in
-    show (sum gearRatios)
+    [g | g <- map gearRatio starLocs, g /= 0]
